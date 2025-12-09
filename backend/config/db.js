c77b8b7fb74e1554dd6044fc94f9ab0a92af00dd
upkar sisodiya <upkarsisodiya@gmail.com>
2025-12-09 23:03:08 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const connection = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "upkar@2003",
+  password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "flipr_db",
   waitForConnections: true,
   connectionLimit: 10,
